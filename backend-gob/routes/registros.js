df85579561fc53cc6893f61277d25140f1a7461d
Moy_Gz04 <jgomezaispuro@gmail.com>
2026-05-19 17:51:05 -0600

    Generación de SPDG V 1.3

--- backend-gob/routes/registros.js
+++ backend-gob/routes/registros.js
@@ -664,4 +664,138 @@ router.delete("/:codigo", async (req, res) => {
   }
 });
 
+/* =========================
+   GUARDAR SPG PDF
+========================= */
+
+router.put(
+
+    "/spg/:codigo",
+
+    async(req,res)=>{
+
+        try{
+
+            const codigo =
+            req.params.codigo.trim();
+
+            const {
+
+                spg_pdf
+
+            } = req.body;
+
+            /* =========================
+               VALIDAR
+            ========================= */
+
+            if(!spg_pdf){
+
+                return res.status(400).json({
+
+                    ok:false,
+
+                    error:
+                    "URL SPG requerida"
+
+                });
+
+            }
+
+            /* =========================
+               EXISTE
+            ========================= */
+
+            const validar =
+            await pool.query(
+
+                `
+
+                SELECT id
+
+                FROM registros
+
+                WHERE codigo = $1
+
+                `,
+
+                [codigo]
+
+            );
+
+            if(
+
+                validar.rows.length === 0
+
+            ){
+
+                return res.status(404).json({
+
+                    ok:false,
+
+                    error:
+                    "Registro no encontrado"
+
+                });
+
+            }
+
+            /* =========================
+               GUARDAR URL
+            ========================= */
+
+            await pool.query(
+
+                `
+
+                UPDATE registros
+
+                SET
+
+                    spg_pdf = $1
+
+                WHERE codigo = $2
+
+                `,
+
+                [
+
+                    spg_pdf,
+
+                    codigo
+
+                ]
+
+            );
+
+            res.json({
+
+                ok:true,
+
+                msg:
+                "SPG guardado"
+
+            });
+
+        }
+
+        catch(error){
+
+            console.log(error);
+
+            res.status(500).json({
+
+                ok:false,
+
+                error:
+                "Error guardando SPG"
+
+            });
+
+        }
+
+    }
+
+);
+
 module.exports = router;
