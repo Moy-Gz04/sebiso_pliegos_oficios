557575d3820cc0f3afef2f000830b23f9d3cf874
Moy_Gz04 <jgomezaispuro@gmail.com>
2026-07-10 10:23:17 -0600

    REPORTES-VIATICOS 1.4

---
+++ js-viaticos/config.js
@@ -0,0 +1,5 @@
+// Producción
+const API_URL = "https://sebiso-pliegos-oficios-1.onrender.com";
+
+// Si algún día vuelves a trabajar localmente solo cambias esta línea.
+// const API_URL = "http://localhost:3000";
--- js-viaticos/viaticos.js
+++ js-viaticos/viaticos.js
@@ -4,28 +4,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+const API = "https://sebiso-pliegos-oficios-1.onrender.com";
+
 async function cargarUnidades(){
 
     const select=document.getElementById("unidad");
 
-    const respuesta=await fetch("/api/viaticos/unidades");
+    const respuesta=await fetch(`${API}/api/viaticos/unidades`);
 
     const datos=await respuesta.json();
 
-    select.innerHTML="<option value=''>Seleccione...</option>";
+    select.innerHTML="<option value=''>Seleccione un área...</option>";
 
     datos.forEach(u=>{
 
         select.innerHTML+=`
-
             <option value="${u.id}">
-
                 ${u.clave} - ${u.nombre}
-
             </option>
-
         `;
 
     });
 
 }
+
+document
+.getElementById("unidad")
+.addEventListener("change",(e)=>{
+
+    sessionStorage.setItem("unidad_id",e.target.value);
+
+});
